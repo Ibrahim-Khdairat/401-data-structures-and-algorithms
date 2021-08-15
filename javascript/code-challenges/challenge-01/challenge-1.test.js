93e376f032ef6dc1462dee5f073ea5c1a7285054
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-08-15 21:59:08 +0300

    update

--- javascript/code-challenges/challenge-01/challenge-1.test.js
+++ javascript/code-challenges/challenge-01/challenge-1.test.js
@@ -24,7 +24,5 @@ describe('Test challenge 01' , ()=>{
     it ('Should return reversed array', ()=>{
         expect(reverseArray([1, 2, 3, 4, 5, 6])).toEqual([6, 5, 4, 3, 2, 1]);
         expect(reverseArray([89, 2354, 3546, 23, 10, -923, 823, -12])).toEqual([-12, 823, -923, 10, 23, 3546, 2354, 89]);
-
-
     })
 })
